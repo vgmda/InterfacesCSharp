0c7b09708076b0201b18134d7419d8c85046b8a8
vgmda <vasilegancin@gmail.com>
2022-06-28 23:41:51 +0100

    Commit

---
+++ DemoLibrary/CustomerModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DemoLibrary;
+
+public class CustomerModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string StreetAddress { get; set; }
+    public string City { get; set; }
+    public string EmailAddress { get; set; }
+    public string PhoneNumber { get; set; }
+}
+
---
+++ InterfacesCSharp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleUI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<PhysicalProductModel> cart = AddSampleData();
+        CustomerModel customer = GetGustomer();
+
+        foreach (PhysicalProductModel prod in cart)
+        {
+            prod.ShipItem(customer);
+        }
+
+        Console.ReadLine();
+    }
+
+    private static CustomerModel GetCustomer()
+    {
+        return new CustomerModel
+        {
+            FirstName = "Test",
+            LastName = "Test 2",
+            City = "Dublin",
+            EmailAddress = "test@test.com",
+            PhoneNumber = "3123123"
+        };
+    }
+}
