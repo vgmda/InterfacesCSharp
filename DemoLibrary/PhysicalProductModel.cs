47b16721d983a04e160d04872a89fa7f5a8ac258
vgmda <vasilegancin@gmail.com>
2022-06-28 23:51:46 +0100

    New file

---
+++ DemoLibrary/PhysicalProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace DemoLibrary;
+
+public class PhysicalProductModel
+{
+    public string Title { get; set; }
+    public bool HasOrderBeenCompleted { get; set; }
+
+    public void ShipItem(CustomerModel customer)
+    {
+        if (HasOrderBeenCompleted == false)
+        {
+            Console.WriteLine($"Simulating shipping {Title} to {customer.FirstName} in {customer.City}");
+            HasOrderBeenCompleted = true;
+        }
+    }
+}
+
