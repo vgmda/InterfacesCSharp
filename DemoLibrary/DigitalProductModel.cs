cfecb19262977350f6722cc7a83567f1bef50463
vgmda <vasilegancin@gmail.com>
2022-06-29 23:36:11 +0100

    New file

---
+++ DemoLibrary/DigitalProductModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DemoLibrary;
+
+public class DigitalProductModel
+{
+    public string Title { get; set; }
+
+    public void ShipItem(CustomerModel customer)
+    {
+        Console.WriteLine($"Simulating emailing {Title} to {customer.EmailAddress}");
+    }
+}
+
