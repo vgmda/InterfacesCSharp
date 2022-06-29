e87860ee56562ad7ad0058c9baff35669b714295
vgmda <vasilegancin@gmail.com>
2022-06-30 00:01:44 +0100

    New digital products

--- InterfacesCSharp/Program.cs
+++ InterfacesCSharp/Program.cs
@@ -6,10 +6,11 @@ public class Program
 {
     static void Main()
     {
-        List<PhysicalProductModel> cart = AddSampleData();
+        // Input more than one type of data (Multiple classes)
+        List<IProductModel> cart = AddSampleData();
         CustomerModel customer = GetCustomer();
 
-        foreach (PhysicalProductModel prod in cart)
+        foreach (IProductModel prod in cart)
         {
             prod.ShipItem(customer);
         }
@@ -22,21 +23,28 @@ public class Program
         return new CustomerModel
         {
             FirstName = "Test",
-            LastName = "Test 2",
+            LastName = "Tester",
             City = "Dublin",
-            EmailAddress = "test@test.com",
-            PhoneNumber = "3123123"
+            EmailAddress = "tester@hmail.com",
+            PhoneNumber = "0892291111"
         };
     }
 
-    private static List<PhysicalProductModel> AddSampleData()
+    private static List<IProductModel> AddSampleData()
     {
-        List<PhysicalProductModel> output = new List<PhysicalProductModel>();
+        List<IProductModel> output = new List<IProductModel>();
 
+        // Physical Products
         output.Add(new PhysicalProductModel { Title = "RTX 3090" });
         output.Add(new PhysicalProductModel { Title = "Dublin T-Shirt" });
         output.Add(new PhysicalProductModel { Title = "SSD 1Tb" });
         output.Add(new PhysicalProductModel { Title = "iPhone 15 Pro 5Tb" });
+
+        // Digital Products
+        output.Add(new DigitalProductModel { Title = "Lesson Source Code" });
+        output.Add(new DigitalProductModel { Title = "Game Key Activation" });
+        output.Add(new DigitalProductModel { Title = "Monthly Subscription Key" });
+
 
         return output;
     }
